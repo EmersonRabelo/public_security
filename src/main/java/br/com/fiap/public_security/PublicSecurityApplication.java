f9575437c68bc47f63bdb9d1751fc0994fe87f39
EmersonRabelo <emersonrabelo1998@gmail.com>
2024-05-27 00:43:41 -0300

    Initial project structure

---
+++ src/main/java/br/com/fiap/public_security/PublicSecurityApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.public_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/public_security/PublicSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.public_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
